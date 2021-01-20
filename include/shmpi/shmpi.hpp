08cdcfcadf1f021eeb12d92fd9dfb63a0e132b2c
mutsuki <mutsuki@momo86.net>
2021-01-20 18:31:53 +0900

    Update the argument of `shmpi::buffer::read_from_device/write_to_device`

--- include/shmpi/shmpi.hpp
+++ include/shmpi/shmpi.hpp
@@ -16,10 +16,10 @@ public:
 	virtual void* get_ptr(const unsigned buffer_id) const = 0;
 
 	// Get data from device
-	virtual void read_from_device(const unsigned buffer_id, const std::size_t offset, const std::size_t count) = 0;
+	virtual void read_from_device(const unsigned buffer_id, const std::size_t mem_offset, const std::size_t count, const std::size_t buffer_offset = 0lu) = 0;
 
 	// Send data to device
-	virtual void write_to_device(const unsigned buffer_id, const std::size_t offset, const std::size_t count) = 0;
+	virtual void write_to_device(const unsigned buffer_id, const std::size_t mem_offset, const std::size_t count, const std::size_t buffer_offset = 0lu) = 0;
 
 	std::size_t get_buffer_count() const {return buffer_count;}
 };
